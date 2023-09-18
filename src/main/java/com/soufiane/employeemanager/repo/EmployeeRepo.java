a8c39800ffd6e8318db2107427b764e876933f21
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-09-18 22:27:16 +0100

    find all+update+delete by Id + find by Id

--- src/main/java/com/soufiane/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/soufiane/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.soufiane.employeemanager.repo;
 import com.soufiane.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/soufiane/employeemanager/service/EmployeeService.java
+++ src/main/java/com/soufiane/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.soufiane.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -18,5 +19,21 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee findEmployee(Long id){
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by Id " + id + "Not found"));
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public void delete(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
